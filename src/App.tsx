3d24779f465e089925b675568ec6b4f44e172f2b
wise-xingxiang <xingxiang.ong@transferwise.com>
2022-07-15 12:03:20 +0800

    background color, table, add assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,43 @@
 import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import "./App.css";
 import DeliverooLogo from "./assets/deliveroo_logo.png";
-import { transformToWhiteOnTransparent } from "./lib/image_transformation";
+import {
+  transformToBlackOnWhite,
+  transformToWhiteOnTransparent,
+} from "./lib/image_transformation";
 
 function App() {
-  const originalRef = useRef<HTMLCanvasElement | null>(null);
-  const editedRef = useRef<HTMLCanvasElement | null>(null);
+  const originalRef = useRef<HTMLCanvasElement>(null);
+  const editedOneRef = useRef<HTMLCanvasElement>(null);
+  const editedTwoRef = useRef<HTMLCanvasElement>(null);
 
+  const [filename, setFilename] = useState<string>();
   const [imageUrl, setImageUrl] = useState("");
-  const [downloadUrl, setDownloadUrl] = useState("");
+  const [downloadOneUrl, setDownloadOneUrl] = useState("");
+  const [downloadTwoUrl, setDownloadTwoUrl] = useState("");
 
   const handleImageUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const imageFile = e.target.files?.item(0);
     const imageFileUrl = URL.createObjectURL(imageFile as Blob);
-    imageFile?.arrayBuffer;
+    setFilename(imageFile?.name.split(".").at(0));
 
     setImageUrl(imageFileUrl);
 
+    // Draw original image
     if (originalRef.current) {
       const originalCanvas = originalRef.current;
       const originalContext = originalCanvas.getContext("2d");
 
       if (originalContext) {
+        // Clear canvas
+        originalContext.clearRect(
+          0,
+          0,
+          originalCanvas.width,
+          originalCanvas.height
+        );
+
+        // Draw new image onto cavas
         const originalImg = new Image();
         originalImg.crossOrigin = "anonymous";
         originalImg.src = imageFileUrl;
@@ -31,10 +47,14 @@ function App() {
     }
 
     // Draw edited image
-    if (editedRef.current) {
-      const editedCanvas = editedRef.current;
+    if (editedOneRef.current) {
+      const editedCanvas = editedOneRef.current;
       const editedContext = editedCanvas.getContext("2d");
       if (editedContext) {
+        // Clear canvas
+        editedContext.clearRect(0, 0, editedCanvas.width, editedCanvas.height);
+
+        // Draw new image onto cavas
         const editedImg = new Image();
         editedImg.crossOrigin = "anonymous";
         editedImg.src = imageFileUrl;
@@ -52,7 +72,43 @@ function App() {
 
           const dataURL = editedCanvas.toDataURL("image/png", 1);
           console.log(dataURL);
-          setDownloadUrl(dataURL);
+          setDownloadOneUrl(dataURL);
+        };
+      }
+    }
+
+    // Draw editedTwo image (black on white background)
+    if (editedTwoRef.current) {
+      const editedTwoCanvas = editedTwoRef.current;
+      const editedTwoContext = editedTwoCanvas.getContext("2d");
+      if (editedTwoContext) {
+        // Clear canvas
+        editedTwoContext.clearRect(
+          0,
+          0,
+          editedTwoCanvas.width,
+          editedTwoCanvas.height
+        );
+
+        // Draw new image onto cavas
+        const editedTwoImg = new Image();
+        editedTwoImg.crossOrigin = "anonymous";
+        editedTwoImg.src = imageFileUrl;
+        editedTwoImg.onload = () => {
+          editedTwoContext.drawImage(editedTwoImg, 0, 0);
+          const imageData = editedTwoContext.getImageData(
+            0,
+            0,
+            editedTwoCanvas.width,
+            editedTwoCanvas.height
+          );
+          const pixels = imageData.data;
+          transformToBlackOnWhite(pixels);
+          editedTwoContext.putImageData(imageData, 0, 0);
+
+          const dataURL = editedTwoCanvas.toDataURL("image/png", 1);
+          console.log(dataURL);
+          setDownloadTwoUrl(dataURL);
         };
       }
     }
@@ -60,15 +116,73 @@ function App() {
 
   return (
     <div className="App">
-      <canvas id="original" width="350" height="350" ref={originalRef} />
-      <canvas id="edited" width="350" height="350" ref={editedRef} />
-      <p>{imageUrl}</p>
-      <input type="file" onChange={handleImageUpload} accept="image/*" />
-      {downloadUrl && (
-        <a href={downloadUrl} download="edited_image">
-          Download
-        </a>
-      )}
+      <table>
+        <thead>
+          <tr>
+            <th>Original</th>
+            <th>White fg + Transparent bg</th>
+            <th>Black fg + White bg</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <canvas
+                id="original"
+                width="350"
+                height="350"
+                ref={originalRef}
+              />
+            </td>
+
+            <td>
+              <canvas
+                id="editedOne"
+                width="350"
+                height="350"
+                ref={editedOneRef}
+              />
+            </td>
+
+            <td>
+              <canvas
+                id="editedTwo"
+                width="350"
+                height="350"
+                ref={editedTwoRef}
+              />
+            </td>
+          </tr>
+
+          <tr>
+            <td></td>
+            <td>
+              {downloadOneUrl && (
+                <a
+                  href={downloadOneUrl}
+                  download={filename + "_white_transparent"}
+                >
+                  Download
+                </a>
+              )}
+            </td>
+            <td>
+              {" "}
+              {downloadTwoUrl && (
+                <a href={downloadTwoUrl} download={filename + "_black_white"}>
+                  Download
+                </a>
+              )}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <input
+        type="file"
+        onChange={handleImageUpload}
+        accept="image/png, image/jpg"
+      />
     </div>
   );
 }
--- src/lib/image_transformation.ts
+++ src/lib/image_transformation.ts
@@ -10,14 +10,38 @@ export const transformToWhiteOnTransparent = (
 
     // If a > 0.1 or rgb >= (240, 240, 240), then set a == 1
     // Else, set rgba to (255, 255, 255, 0)
-
-    if (isCloseToWhite(red, green, blue)) {
-      pixels[i + 3] = 0;
+    if (alpha <= 0.1 || isCloseToWhite(red, green, blue)) {
+      pixels[i + 3] = 0; // Make transparent
     } else {
       pixels[i] = 255;
       pixels[i + 1] = 255;
       pixels[i + 2] = 255;
-      // pixels[i + 3] = 255;
+      pixels[i + 3] = 255; // Zero opacity, Full alpha
+    }
+  }
+};
+
+export const transformToBlackOnWhite = (pixels: Uint8ClampedArray): void => {
+  // Note: The input pixels are expected to be the original image file uploaded by the user, instead of the "white on transparent" one.
+  // Modify pixels in-place.
+  for (let i = 0; i < pixels?.length; i += 4) {
+    let red = pixels[i];
+    let green = pixels[i + 1];
+    let blue = pixels[i + 2];
+    let alpha = pixels[i + 3];
+
+    // If a > 0.1 or rgb >= (240, 240, 240), then set rgba to (255, 255, 255, 255)
+    // Else, set rgba to (0, 0, 0, 255)
+    if (alpha <= 0.1 || isCloseToWhite(red, green, blue)) {
+      pixels[i] = 255;
+      pixels[i + 1] = 255;
+      pixels[i + 2] = 255;
+      pixels[i + 3] = 255; // Zero opacity, Full alpha
+    } else {
+      pixels[i] = 0;
+      pixels[i + 1] = 0;
+      pixels[i + 2] = 0;
+      pixels[i + 3] = 255; // Zero opacity, Full alpha
     }
   }
 };
