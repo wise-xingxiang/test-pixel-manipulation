aac40c6a7f054639e2eea1e7bb8c3108ec29743c
wise-xingxiang <xingxiang.ong@transferwise.com>
2022-07-19 12:33:24 +0800

    add filetype checking

--- src/components/CropImage/CropImage.tsx
+++ src/components/CropImage/CropImage.tsx
@@ -3,6 +3,7 @@ import Cropper, { ReactCropperElement } from "react-cropper";
 import "./CropImage.css";
 import "cropperjs/dist/cropper.css";
 import { IMAGE_MIN_HEIGHT_PX, IMAGE_MIN_WIDTH_PX } from "../../lib/constants";
+import { getFileType } from "../../lib/filetype";
 
 interface Props {
   cropConfirmCallback: (
@@ -14,18 +15,29 @@ interface Props {
 const CropImage = ({ cropConfirmCallback }: Props) => {
   const [imageSrc, setImageSrc] = useState<string>();
   const [filename, setFilename] = useState<string>();
+  const [fileType, setFileType] = useState<string>();
 
   const cropperRef = useRef<ReactCropperElement>(null);
 
   const onFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files.length > 0) {
       const file = e.target.files[0];
-      setFilename(file.name.split(".").at(0));
-      const reader = new FileReader();
-      reader.addEventListener("load", () =>
-        setImageSrc(reader?.result as string)
-      );
-      reader.readAsDataURL(file);
+
+      // Validate uploaded file
+      getFileType(file, (s) => {
+        if (!s) {
+          alert("Only png and jpg files are accepted");
+          // e.target.value = e.target.defaultValue; // Reset the input
+        } else {
+          console.log("Image uploaded was of type", s);
+          setFilename(file.name.split(".").at(0));
+          const reader = new FileReader();
+          reader.addEventListener("load", () =>
+            setImageSrc(reader?.result as string)
+          );
+          reader.readAsDataURL(file);
+        }
+      });
     }
   };
 
@@ -45,10 +57,13 @@ const CropImage = ({ cropConfirmCallback }: Props) => {
       <main>
         {imageSrc ? (
           <div className="container">
-            <div className="cropContainer">
+            <div
+              className="cropContainer"
+              style={{ display: "flex", justifyContent: "center" }}
+            >
               <Cropper
                 src={imageSrc}
-                style={{ height: "400", width: "100%" }}
+                style={{ height: "400px", width: "100%" }}
                 // Cropper.js options
                 aspectRatio={IMAGE_MIN_WIDTH_PX / IMAGE_MIN_HEIGHT_PX}
                 dragMode="move"
@@ -64,6 +79,12 @@ const CropImage = ({ cropConfirmCallback }: Props) => {
                 style={{ marginTop: "1rem", marginBottom: "2rem" }}
               >
                 Confirm crop
+              </button>
+              <button
+                onClick={() => setImageSrc(undefined)}
+                style={{ marginLeft: "2rem" }}
+              >
+                Reset
               </button>
             </div>
           </div>
